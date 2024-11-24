3c990ae32a49be92817d77a27a41795e8e5ff767
Jacob Wingate <jacob.wingate33@gmail.com>
2024-11-24 13:12:05 +0100

    add while and do while loops

--- src/main/kotlin/com/kotlinplayground/basics/LoopsAndRanges.kt
+++ src/main/kotlin/com/kotlinplayground/basics/LoopsAndRanges.kt
@@ -9,4 +9,24 @@ fun main() {
     for (i in reverseRange) println(i)
 
     for (i in reverseRange step 2) println("every second step: $i")
+
+    fun exploreWhile() {
+        var x = 1
+        while (x < 5) {
+            println("Value of x: $x")
+            x++
+        }
+    }
+    exploreWhile()
+
+    exploreDoWhile()
 }
+
+fun exploreDoWhile() {
+    var i = 0
+    do{
+        println("Value of I: $i")
+        i++
+    } while (i < 5)
+}
+
