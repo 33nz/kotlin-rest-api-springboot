29f3df6c82e597e06a38a6590b7fb81117f124b5
Jacob Wingate <jacob.wingate33@gmail.com>
2024-11-24 12:34:18 +0100

    add final var examples

--- src/main/kotlin/com/kotlinplayground/basics/Variables.kt
+++ src/main/kotlin/com/kotlinplayground/basics/Variables.kt
@@ -1,13 +1,28 @@
 package com.kotlinplayground.basics
 
 fun main() {
-     val name : String = "Jake"
+     val name = "Jake"
     println(name)
 
     //name = "Jacob"
 
-    var age: Int = 41
+    var age = 41
     println(age)
     age = 42
     println(age)
+
+    val salary = 3000L
+    println(salary)
+
+    val course = "Kotlin Spring"
+    println("course: $course and the course length is ${course.length}")
+
+    val multiLine = "ABC \nDEF"
+    println(multiLine)
+
+    val multiLine1 = """
+        |ABC
+        |DEF
+    """.trimMargin()
+    println(multiLine1)
 }
