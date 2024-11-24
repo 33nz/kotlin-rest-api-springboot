663a5620612a9c7ad871352319140865b91dc729
Jacob Wingate <jacob.wingate33@gmail.com>
2024-11-24 13:02:49 +0100

    add ranges examples

---
+++ src/main/kotlin/com/kotlinplayground/basics/LoopsAndRanges.kt
@@ -0,0 +1,12 @@
+package com.kotlinplayground.basics
+
+fun main() {
+    val range = 1..10
+
+    for (i in range) println("i: $i")
+
+    val reverseRange = 10 downTo 1
+    for (i in reverseRange) println(i)
+
+    for (i in reverseRange step 2) println("every second step: $i")
+}
