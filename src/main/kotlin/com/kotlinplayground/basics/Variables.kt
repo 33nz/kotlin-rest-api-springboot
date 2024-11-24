8d5236129b4785d4a4b5496b0a3cd3d6451cd480
Jacob Wingate <jacob.wingate33@gmail.com>
2024-11-24 12:11:29 +0100

    add varables examples and new file structure

---
+++ src/main/kotlin/com/kotlinplayground/basics/Variables.kt
@@ -0,0 +1,13 @@
+package com.kotlinplayground.basics
+
+fun main() {
+     val name : String = "Jake"
+    println(name)
+
+    //name = "Jacob"
+
+    var age: Int = 41
+    println(age)
+    age = 42
+    println(age)
+}
