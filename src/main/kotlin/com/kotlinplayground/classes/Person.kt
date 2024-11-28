09ecd30a6354ec0f551aae7210dfa9a8f6515ed2
Jacob Wingate <jacob.wingate33@gmail.com>
2024-11-28 21:06:34 +0100

    add constructors and default arguments

---
+++ src/main/kotlin/com/kotlinplayground/classes/Person.kt
@@ -0,0 +1,19 @@
+package com.kotlinplayground.classes
+
+class Person(val name: String = "", val age: Int = 0) {
+
+    fun action() {
+        println("Person Walks")
+    }
+
+}
+
+fun main() {
+    val person = Person("Alex", 25)
+    person.action()
+    println("Name : ${person.name} and the age is ${person.age}")
+
+    val person1 = Person()
+    println("Name : ${person1.name} and the age is ${person1.age}")
+
+}
