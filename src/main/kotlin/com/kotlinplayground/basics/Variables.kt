69fb2dadadadfa5685f7acb639540dd275dd794c
Jacob Wingate <jacob.wingate33@gmail.com>
2024-11-28 20:42:45 +0100

    add top level functions and properties

--- src/main/kotlin/com/kotlinplayground/basics/Variables.kt
+++ src/main/kotlin/com/kotlinplayground/basics/Variables.kt
@@ -1,5 +1,8 @@
 package com.kotlinplayground.basics
 
+import com.kotlinplayground.functions.courseName
+import com.kotlinplayground.functions.topLevelFunction
+
 fun main() {
      val name = "Jake"
     println(name)
@@ -25,4 +28,9 @@ fun main() {
         |DEF
     """.trimMargin()
     println(multiLine1)
+
+    val num = topLevelFunction()
+    println("Num is : $num")
+
+    println("Course name : $courseName")
 }
