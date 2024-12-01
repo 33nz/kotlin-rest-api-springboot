4757ea6e8ad088b7b5da2c52fe11f24869a2d892
Jacob Wingate <jacob.wingate33@gmail.com>
2024-12-01 12:09:42 +0100

    add getter and setter code

--- src/main/kotlin/com/kotlinplayground/classes/Item.kt
+++ src/main/kotlin/com/kotlinplayground/classes/Item.kt
@@ -2,7 +2,21 @@ package com.kotlinplayground.classes
 
 class Item {
 
-    var name : String = ""
+    var name: String = ""
+    var price: Double = 0.0
+        get() {
+            println("Inside Getter")
+            return field
+        }
+        set(value) {
+            if(value >= 0.0) {
+                println("Inside Setter")
+                field = value
+            } else {
+                throw IllegalArgumentException("Negative Price is not Allowed!")
+            }
+        }
+
 
     constructor(_name: String) : this() {
         name = _name
@@ -18,6 +32,6 @@ fun main() {
 
     item.name = "Iphone 12"
     println("Item is ${item.name}")
-
-    println(item)
+    item.price = 12.33
+    println(item.price)
 }
