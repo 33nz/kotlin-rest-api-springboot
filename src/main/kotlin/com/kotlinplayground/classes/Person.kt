87c612314d113eebd0f7fb04ec5549b61d2e1005
Jacob Wingate <jacob.wingate33@gmail.com>
2024-12-01 11:09:52 +0100

    add init block

--- src/main/kotlin/com/kotlinplayground/classes/Person.kt
+++ src/main/kotlin/com/kotlinplayground/classes/Person.kt
@@ -3,6 +3,13 @@ package com.kotlinplayground.classes
 class Person(val name: String = "", val age: Int = 0) {
 
     var email : String = ""
+    var nameLength: Int = 0
+    init {
+        println("inside init block")
+        nameLength = name.length
+    }
+
+
     constructor(_email : String,
         _name: String = "",
         _age: Int = 0) : this(_name, _age) {
@@ -24,6 +31,6 @@ fun main() {
     println("Name : ${person1.name} and the age is ${person1.age}")
 
     val person2 = Person(_email = "jake@gmail.com", "jake", 33)
-    println("Name : ${person2.name} and the age is ${person2.age} and the email is ${person2.email}")
+    println("Name : ${person2.name} and the age is ${person2.age} and the email is ${person2.email} and the nameLength ${person2.nameLength}")
 
 }
